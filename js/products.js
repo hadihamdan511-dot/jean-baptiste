499e815ab935c57f0e02e544b977ffccc50255fa
hadihamdan511-dot <hadihamdan511@gmail.com>
2026-09-06 20:37:38 +0300

    need

--- js/products.js
+++ js/products.js
@@ -476,7 +476,7 @@ const PRODUCTS = [
   {
     "id": 44,
     "name": "Cool Water Davidoff",
-    "image": "images/products/046_Cool Water Davidoff.jpeg",
+    "image": "images/products/046_Cool Water Davidoff.png.jpeg",
     "section": "men",
     "bestseller": true,
     "vip": false,
@@ -2159,7 +2159,7 @@ const PRODUCTS = [
   {
     "id": 197,
     "name": "Cool Water Woman Davidoff",
-    "image": "images/products/184_Cool Water Woman Davidoff.png",
+    "image": "images/products/184_Cool Water Woman Davidoff.png.jpeg",
     "section": "women",
     "bestseller": false,
     "vip": false,
