c61d95fb97ebec5fcdfa3709dacc6c6ac9b1be94
hadihamdan511-dot <hadihamdan511@gmail.com>
2026-09-13 16:08:28 +0300

    Update products.js

--- js/products.js
+++ js/products.js
@@ -113,7 +113,7 @@ const PRODUCTS = [
   {
     "id": 11,
     "name": "Legend Eau de Parfum Montblanc",
-    "image": "images/products/010_Legend Eau de Parfum Montblanc.png",
+    "image": "images/products/010_Legend Eau de Parfum Montblanc.jpeg",
     "section": "men",
     "bestseller": false,
     "vip": false,
