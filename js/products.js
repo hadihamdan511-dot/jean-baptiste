32e7b33aba9cf16bb786a56104b53259b5ac8b80
hadihamdan511-dot <hadihamdan511@gmail.com>
2026-09-13 16:59:36 +0300

    Update products.js

--- js/products.js
+++ js/products.js
@@ -113,7 +113,7 @@ const PRODUCTS = [
   {
     "id": 11,
     "name": "Legend Montblanc",
-    "image": "images/products/1.jpeg",
+    "image": "images/products/010_Legend Montblanc.jpeg",
     "section": "men",
     "bestseller": false,
     "vip": false,
